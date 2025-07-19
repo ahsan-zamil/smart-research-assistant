ea3af82a0023a0ab6b164bc3e3021d34c7372207
Ahsan Zamil <ahsanzamil76@gmail.com>
2025-07-19 12:16:02 +0530

    response class updated

--- src/main/java/com/research/assistant/GeminiResponse.java
+++ src/main/java/com/research/assistant/GeminiResponse.java
@@ -1,4 +1,28 @@
 package com.research.assistant;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
 public class GeminiResponse {
+    private List<Candidate> candidates;
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Candidate {
+        private Content content;
+    }
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Content {
+        private List<Part> parts;
+    }
+
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Part {
+        private String text;
+    }
 }
